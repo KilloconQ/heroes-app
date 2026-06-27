5acec026251e9632588948b3ee5567c0d7ae395a
Killoconq <fjc1997v@gmail.com>
2026-06-26 18:24:55 -0600

    cosas

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,44 +1,43 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter } from "react-router";
 
-import { AdminLayout } from '@/admin/layouts/AdminLayout';
-import { AdminPage } from '@/admin/pages/AdminPage';
-import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
-import { HeroPage } from '@/heroes/pages/hero/HeroPage';
-import { HomePage } from '@/heroes/pages/home/HomePage';
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 // import SearchPage from '@/heroes/pages/search/SearchPage';
 
-const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
-const AdminPage = lazy(() => import('@/admin/pages/AdminPage'));
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
+const AdminPage = lazy(() => import("@/admin/pages/AdminPage"));
+const HomePage = lazy(() => import("@/heroes/pages/home/HomePage"));
+const HeroPage = lazy(() => import("@/heroes/pages/hero/HeroPage"));
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HeroesLayout />,
     children: [
       {
         index: true,
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
-        path: '/hero/1',
-        element: <HeroPage />
+        path: "/hero/1",
+        element: <HeroPage />,
       },
       {
-        path: '/search',
-        element: <SearchPage />
-      }
-    ]
+        path: "/search",
+        element: <SearchPage />,
+      },
+    ],
   },
   {
-    path: '/admin',
+    path: "/admin",
     element: <AdminLayout />,
     children: [
       {
         index: true,
-        element: <AdminPage />
-      }
-    ]
-  }
+        element: <AdminPage />,
+      },
+    ],
+  },
 ]);
